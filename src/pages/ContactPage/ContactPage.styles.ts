1a7b34f42a22563d55ebfe7b3ef677c36306c020
emanuelhenrique-dev <emanuelhenriquefs@gmail.com>
2025-09-20 17:38:42 -0300

    feat: implement ContactPage

--- src/pages/ContactPage/ContactPage.styles.ts
+++ src/pages/ContactPage/ContactPage.styles.ts
@@ -1,37 +1,183 @@
 import styled from 'styled-components';
+import { TextBox } from '../../components/Login/Login.style';
 
 export const ContactPageContainer = styled.main`
   width: 100%;
-  max-width: var(--max-width);
+  /* max-width: var(--max-width);
   margin: 0 auto;
-  padding: 30px 20px;
+  padding: 30px 20px; */
 
-  height: calc(100vh - 175px);
+  display: flex;
+  height: 100vh;
+  max-height: calc(100vh - 175px);
+  background-image: url(/job-5382501_1920.jpg);
+  background-size: cover;
 
-  .back-button {
-    margin-bottom: 12px;
+  @media (max-width: 840px) {
+    height: fit-content;
+    max-height: 100%;
+  }
+`;
 
-    button {
+export const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  .Heading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+
+    background-color: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(5px);
+
+    h2 {
+      font-family: 'Source Serif 4';
+      font-weight: 500;
+      font-size: 3rem;
+      line-height: 133%;
+      color: ${(props) => props.theme.colors['white']};
+    }
+  }
+
+  .main-content {
+    height: 100%;
+    flex: 1;
+    background-color: ${(props) => props.theme.colors['background']};
+
+    .wrapper {
       display: flex;
-      align-items: center;
-      gap: 12px;
-      margin-left: auto;
+      max-width: var(--max-width);
+      height: 100%;
+      margin: 0 auto;
+      padding: 30px 20px;
 
-      font-family: 'Noto Sans';
-      font-weight: 400;
-      font-size: 1rem;
-      line-height: 175%;
-      color: ${(props) => props.theme.colors['base-subtitle']};
+      .hero {
+        position: relative;
+        flex: 1;
+        background-image: url(/call-center-7040784_1280.png);
+        background-size: 400px;
+        background-repeat: no-repeat;
+        background-position: center;
 
-      background: transparent;
+        filter: drop-shadow(10px 26px 2px rgba(0, 0, 0, 0.25));
 
-      &:hover {
-        color: ${(props) => props.theme.colors['blue-primary']};
+        > div {
+          width: 100%;
+          position: absolute;
+          bottom: 12px;
+          left: 50%;
+
+          transform: translate(-50%);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 12px;
+
+          span {
+            display: flex;
+            align-items: center;
+            gap: 5px;
+
+            font-weight: 400;
+            font-size: 1rem;
+            line-height: 156%;
+            color: ${(props) => props.theme.colors['blue-primary']};
+          }
+        }
+      }
+
+      .form-container {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+
+  @media (max-width: 840px) {
+    & {
+      .Heading {
+        min-height: 150px;
+      }
+
+      .wrapper {
+        flex-direction: column;
+        gap: 50px;
+        height: fit-content;
+        padding: 30px;
+
+        .hero {
+          min-height: 300px;
+          background-size: 300px !important;
+          background-position: top !important;
+
+          margin-bottom: 50px;
+
+          > div {
+            bottom: -24px !important;
+          }
+        }
+
+        h3 {
+          font-size: 2.3rem !important;
+        }
       }
     }
   }
 `;
 
-export const ContentContainer = styled.main`
-  margin-bottom: 24px;
+export const ContactForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 18px;
+
+  max-width: 400px;
+
+  h3 {
+    font-weight: 600;
+    font-size: 2.5rem;
+    line-height: 100%;
+    text-align: center;
+    color: ${(props) => props.theme.colors['blue-primary']};
+    margin-bottom: 30px;
+  }
+
+  textarea {
+    width: 100%;
+    height: 100px;
+    border: 2px solid ${(props) => props.theme.colors['blue-primary']}; //mudei
+    border-radius: 8px;
+    padding: 14px 16px;
+
+    background: transparent; //mudei
+    color: ${(props) => props.theme.colors['base-title']};
+    resize: none;
+
+    &:focus {
+      outline: none;
+      border-color: ${(props) => props.theme.colors['blue-primary']};
+    }
+  }
+`;
+
+export const TextBoxContact = styled(TextBox)`
+  input {
+    border: 2px solid ${(props) => props.theme.colors['blue-primary']}; //mudei
+
+    background: transparent; //mudei
+
+    &:focus + label,
+    &:not(:placeholder-shown) + label {
+      background: ${(props) => props.theme.colors['background']}; //mudei
+    }
+  }
 `;
--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,22 +1,58 @@
-import { ArrowLeftIcon } from '@phosphor-icons/react';
-
-import { useNavigate } from 'react-router-dom';
-import { ContactPageContainer, ContentContainer } from './ContactPage.styles';
+import { EnvelopeSimpleIcon, PhoneIcon } from '@phosphor-icons/react';
+import {
+  ContactForm,
+  ContactPageContainer,
+  ContentContainer,
+  TextBoxContact
+} from './ContactPage.styles';
+import { Button } from '../../components/Login/Login.style';
 
 export function ContactPage() {
-  const navigate = useNavigate();
-
   return (
     <ContactPageContainer>
-      <div className="back-button">
-        <button onClick={() => navigate(-1)}>
-          <ArrowLeftIcon width={22} />
-          voltar
-        </button>
-      </div>
-
       <ContentContainer>
-        <h2>Contanto</h2>
+        <div className="Heading">
+          <h2>Contate-nos</h2>
+        </div>
+        <div className="main-content">
+          <div className="wrapper">
+            <div className="hero">
+              <div>
+                <span>
+                  <EnvelopeSimpleIcon size={22} />
+                  info@example.com
+                </span>
+                <span>
+                  <PhoneIcon size={22} />
+                  0864003-8547
+                </span>
+              </div>
+            </div>
+            <div className="form-container">
+              <ContactForm>
+                <h3>Entre em contato</h3>
+                <TextBoxContact>
+                  <input required type="text" placeholder="" />
+                  <label>Nome</label>
+                </TextBoxContact>
+
+                <TextBoxContact>
+                  <input required type="text" placeholder="" />
+                  <label>E-mail</label>
+                </TextBoxContact>
+
+                <textarea
+                  required
+                  placeholder="O que você gostaria de dizer?"
+                ></textarea>
+
+                <Button $variant="blue" type="submit">
+                  Enviar Mensagem
+                </Button>
+              </ContactForm>
+            </div>
+          </div>
+        </div>
       </ContentContainer>
     </ContactPageContainer>
   );
